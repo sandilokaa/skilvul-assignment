41fb7bf3a5d3fa5774d8d957d8993d4559aa7765
sanlokaja <sandyloka790@gmail.com>
2022-10-04 23:13:11 +0700

    docs: fixed count

--- 02-TPA-BMI-WEBSITE/assets/scripts/main.js
+++ 02-TPA-BMI-WEBSITE/assets/scripts/main.js
@@ -27,7 +27,7 @@ btnCount.addEventListener('click', function(){
         resultIndicator.innerText = "Obesity"
     } else if (result >= 25 && result <= 29.9){
         resultIndicator.innerText = "Overweight"
-    } else if (result >= 18.5 & result <= 24.9){
+    } else if (result >= 18.5 && result <= 24.9){
         resultIndicator.innerText = "Normal weight"
     } else {
         resultIndicator.innerText = "Underweight"
